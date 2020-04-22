eba2e0f78b22db9c1870e34f0eca05acbc013898
Jason Koolman <jason.koolman@hotmail.com>
2020-04-22 17:47:57 +0200

    feat: Add getCommits method to GitService

--- src/services/GitService.js
+++ src/services/GitService.js
@@ -10,3 +10,14 @@ export function getRepository(repo) {
   const url = `${API_URL}/repos/${repo}`;
   return fetch(url).then(res => res.json());
 }
+
+/**
+ * Get the commits for a repository.
+ *
+ * @param  {String} repo
+ * @return {Promise}
+ */
+export function getCommits(repo) {
+  const url = `${API_URL}/repos/${repo}/commits`;
+  return fetch(url).then(res => res.json());
+}
