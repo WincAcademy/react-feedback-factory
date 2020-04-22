7b507620f1eb634fc64bfbb0ff2a00c496933971
Jason Koolman <jason.koolman@hotmail.com>
2020-04-22 19:38:40 +0200

    refactor: Styling changes, code consistency

--- src/components/shared/Textarea.js
+++ src/components/shared/Textarea.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const Textarea = ({ id, name, value, placeholder, required, disabled, className, onBlur, onChange }) => {
+const Textarea = ({ id, name, value, rows, placeholder, required, disabled, className, onBlur, onChange }) => {
   return (
     <textarea
       id={id}
       name={name}
       value={value}
+      rows={rows}
       className={`input ${className ? className : ''}`}
       placeholder={placeholder}
       required={required}
